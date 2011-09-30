d911d3b79469cd8fec51872dc871b4b436fccc6d
Gatis Tomsons <gatis@ithouse.cc>
2011-09-30 15:02:25 +0100

    fixed bug in helper

--- app/helpers/lolita/i18n_helper.rb
+++ app/helpers/lolita/i18n_helper.rb
@@ -1,7 +1,7 @@
 module Lolita
   module I18nHelper
     def locale_options
-      @@locale_options ||= ::I18n::available_locales.collect{|locale| 
+      ::I18n::available_locales.collect{|locale| 
         [::I18n.t(locale, :default => locale), locale] unless [::I18n.default_locale,@active_locale].include?(locale)
       }.compact.insert(0,[::I18n.t("lolita-i18n.choose-other-language", :default => "Choose other language"),""])
     end
